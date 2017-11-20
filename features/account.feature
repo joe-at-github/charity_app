799e51ca2178380a708102e3159a7b305ee40d98
joeffrey_lamy_3006 <joeffreylamy@hotmail.com>
2017-11-20 10:59:36 +0000

    adds first account test

---
+++ features/account.feature
@@ -0,0 +1,8 @@
+Feature: A visitor can register
+
+Scenario: A visitor can register as a charity
+  Given a visitor is on the welcome page
+  And they click on register
+  And they fill in the account registration form
+  When they submit the form
+  Then a new account is created
---
+++ features/step_definitions/account_steps.rb
@@ -0,0 +1,19 @@
+Given("a visitor is on the welcome page") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given("they click on register") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given("they fill in the account registration form") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("they submit the form") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("a new account is created") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
