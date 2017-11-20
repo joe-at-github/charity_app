1d5574f4e99fa55a402e5758ca38ab67fbe56c01
joeffrey_lamy_3006 <joeffreylamy@hotmail.com>
2017-11-20 17:10:51 +0000

    adds register page and root, commit prior to g devise charity

--- features/account.feature
+++ features/account.feature
@@ -3,6 +3,7 @@ Feature: A visitor can register
 Scenario: A visitor can register as a charity
   Given a visitor is on the welcome page
   And they click on register
+  And they click on charity
   And they fill in the account registration form
   When they submit the form
   Then a new account is created
--- features/step_definitions/account_steps.rb
+++ features/step_definitions/account_steps.rb
@@ -4,14 +4,22 @@ end
 
 Given("they click on register") do
   expect(page).to have_link("Sign up")
+  click_link 'Sign up'
+end
+
+Given("they click on charity") do
+  expect(page).to have_link("charity")
+  click_link 'charity'
 end
 
 Given("they fill in the account registration form") do
-  pending # Write code here that turns the phrase above into concrete actions
+  fill_in 'Charity name', with: 'Todos Together'
+  fill_in 'Email', with: 'todos@mail.com'
+  fill_in 'Password', with: 'todospassword'
 end
 
 When("they submit the form") do
-  pending # Write code here that turns the phrase above into concrete actions
+  click_link 'Submit'
 end
 
 Then("a new account is created") do
