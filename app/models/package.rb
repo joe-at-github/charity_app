6b2a121199e4e190ad38230516f24c5f7d2a90b8
joeffrey_lamy_3006 <joeffreylamy@hotmail.com>
2017-11-24 21:22:09 +0000

    sets relationships and validations on package and package_items

--- app/models/package.rb
+++ app/models/package.rb
@@ -1,3 +1,11 @@
 class Package < ApplicationRecord
   belongs_to :package_status
+  has_many :package_items
+  before_create :set_package_status
+
+  private
+  def set_package_status
+    self.package_status_id = 1
+  end
+
 end
--- app/models/package_item.rb
+++ app/models/package_item.rb
@@ -1,4 +1,14 @@
 class PackageItem < ApplicationRecord
   belongs_to :product
-  belongs_to :order
+  belongs_to :package
+
+  validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validate :package_present
+
+  private
+  def package_present
+    if package.nil?
+      errors.add(:package, "is not a valid package.")
+    end
+  end
 end
